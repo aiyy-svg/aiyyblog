64cba3ad2a30c2750a965ef36d92199c8b0117bb
bonegu <2457335849@qq.com>
2020-03-21 21:04:45 +0800

    home

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+body{
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	height: 100%;
+	background-image: url(../static/img/home.jpg);
+	background-size: cover;
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+   <h1 class="aiyy">Aiyy`s blog</h1>
+   <div  class="box">
+
+   </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ .home{
+ 	width: 100%;
+ 	height: 100%;
+ }
+ .aiyy{
+ 	margin:100px 0 0 150px;
+ 	color: blue;
+ }
+</style>
